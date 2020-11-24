97f7aa70911fe71e00696a60fea8051358bf01f2
Muneeb Baderoen <mbaderoen@gmail.com>
2020-11-25 01:12:38 +0200

    Add implementation for Hanoi with 3 pegs

--- 01_intro.hs
+++ 01_intro.hs
@@ -1,3 +1,5 @@
+import Debug.Trace
+
 -- Testing out function definitions, and learning about the `otherwise` guard
 test_func :: Int -> String
 test_func n
@@ -106,12 +108,72 @@ addDiscsToPeg peg discs = (fst peg, concat [discs, (snd peg)])
 addDiscsToFirstPeg :: [Peg] -> [Disc] -> [Peg]
 addDiscsToFirstPeg (p:pegs) discs = (addDiscsToPeg p discs) : pegs
 
-hanoi :: Integer -> PegLabel -> PegLabel -> PegLabel -> [Move]
-hanoi numDiscs pegA pegB pegC =
-  (hanoiPerform
-    (addDiscsToFirstPeg
-      (generatePegs [pegA, pegB, pegC])
-      (generateDiscs numDiscs)))
+--initializePegs :: Integer -> PegLabel -> PegLabel -> PegLabel -> [Peg]
+--initializePegs numDiscs pegA pegB pegC =
+initializePegs :: Integer -> [Peg]
+initializePegs numDiscs =
+  (addDiscsToFirstPeg
+    (generatePegs ["a", "b", "c"])
+    (generateDiscs numDiscs))
 
-hanoiPerform :: [Peg] -> [Move]
-hanoiPerform pegs = []
+-- Need to get comfortable with maybes
+-- The disc is not guaranteed to exist, but `head` explodes when
+-- there are no elements. I'll wrap this one day to handle the edge
+locateDisc :: Disc -> [Peg] -> Peg
+locateDisc disc pegs = head (filter (\peg -> elem disc (snd peg)) pegs)
+
+-- The peg is not guaranteed to exist, but `head` explodes when
+-- there are no elements. I'll wrap this one day to handle the edge
+locatePegByName :: PegLabel -> [Peg] -> Peg
+locatePegByName pegLabel pegs = head (filter (\peg -> fst peg == pegLabel) pegs)
+
+-- There is only one temp peg right now, but this will come in handy when
+-- I go after the 4+ pegs scenario. Selecting pegs there will need some
+-- intelligence that isn't required when there are only 3 pegs
+tempPegs :: PegLabel -> PegLabel -> [Peg] -> [Peg]
+tempPegs sourcePegLabel destinationPegLabel pegs =
+  (filter (\peg -> fst peg /= sourcePegLabel && fst peg /= destinationPegLabel) pegs)
+
+selectTempPeg :: Peg -> Peg -> [Peg] -> Peg
+selectTempPeg sourcePeg destinationPeg pegs= head (tempPegs (fst sourcePeg) (fst destinationPeg) pegs)
+
+moveDisc :: Disc -> PegLabel -> [Peg] -> [Peg]
+moveDisc disc destinationPegLabel pegs =
+  traceShow ("moveDisc", disc, sourcePegLabel, destinationPegLabel, pegs) map
+  (\p -> if fst sourcePeg == fst p
+           then (fst sourcePeg, tail (snd sourcePeg))
+         else
+           if fst destinationPeg == fst p
+             then (fst destinationPeg, disc:(snd destinationPeg))
+           else p
+  )
+  pegs
+  where sourcePeg = locateDisc disc pegs
+        sourcePegLabel = fst sourcePeg
+        destinationPeg = locatePegByName destinationPegLabel pegs
+        tempPeg = head (tempPegs (fst sourcePeg) (fst destinationPeg) pegs)
+
+-- Learned about where clauses. Those are cool for predefining things that
+-- require some computation. Saves some wasted effort, and allow use within
+-- guard clauses
+hanoiPerform :: Disc -> PegLabel -> [Peg] -> [Peg]
+hanoiPerform disc destinationPegLabel pegs
+    | disc == 1 =
+      moveDisc disc destinationPegLabel pegs
+    | otherwise =
+      hanoiPerform (disc - 1) destinationPegLabel (
+        moveDisc disc destinationPegLabel (
+          hanoiPerform (disc - 1) tempPegLabel pegs
+        )
+      )
+  where sourcePeg = locateDisc disc pegs
+        destinationPeg = locatePegByName destinationPegLabel pegs
+        tempPegLabel = fst (selectTempPeg sourcePeg destinationPeg pegs)
+
+hanoi :: Integer -> [Peg]
+hanoi numDiscs =
+  (hanoiPerform numDiscs destinationPegLabel pegs)
+  where pegs = initializePegs numDiscs
+        destinationPegLabel = (fst (last pegs))
+
+demo = hanoi 3
